6b19938efcbde75eaa2f6a1bb96945a395b0cd98
elek <ceo@elek.ga>
2022-12-03 01:44:43 +0100

    Half done 2021/4, Readme

---
+++ 2021/4/index.js
@@ -0,0 +1,26 @@
+// import fs from "fs";
+
+// let globalINDEX =  0 
+
+// const input = fs.readFileSync("input.txt", "utf-8");
+// const order = input.split("\n")[0].split(",");
+// const boards = input.substring(order.join(",").length).split("\n\n");
+// boards.shift();
+// let tables = new Array();
+// boards.forEach((board) => {
+//   let rows = [];
+//   board.split("\n").forEach((row) => {
+//     rows.push(row.replaceAll("  ", " ").split(" "));
+//   });
+//   tables.push(rows);
+// });
+// const rules = {
+//     didWin:(table)=> {
+//         nows = order.splice(0, globalINDEX)
+        
+
+//     }
+// };
+
+
+// rules.didWin(tables[1])
